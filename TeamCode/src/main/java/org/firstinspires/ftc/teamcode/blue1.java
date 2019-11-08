217025aebb804e84da7e3221c492d8fd24a427a1
PurpleLaptop1 <techturtles10179@gmail.com>
2019-11-07 18:01:07 -0600

    finished blue1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blue1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blue1.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
-public class blue1 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.lib.AutoController;
+import org.firstinspires.ftc.teamcode.lib.Configurator;
+import org.firstinspires.ftc.teamcode.lib.WheelController;
+
+@Autonomous(name="blue1")
+public class blue1 extends LinearOpMode{
+    Configurator config;
+    WheelController wheelController;
+
+    public void runOpMode(){
+        config = new Configurator(this);
+        wheelController = new WheelController(config);
+        Servo foundationGrabber;
+        foundationGrabber = hardwareMap.servo.get("foundationGrabber");
+        waitForStart();
+        wheelController.runWithoutEncoder();
+
+        //moves sideways to accommodate for other bot
+        wheelController.moveXY(0.5,0);
+        sleep(500);
+        wheelController.stopWheels();
+
+        //moves forward under the skybridge
+        wheelController.moveXY(0,0.5);
+        sleep(1250);
+        wheelController.stopWheels();
+    }
 }
