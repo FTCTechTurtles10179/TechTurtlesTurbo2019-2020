4edd69e7f94f27f56ff42cce11eca5dba1a5384b
PurpleLaptop2 <techturtles10179@gmail.com>
2019-11-08 20:31:24 -0600

    UPDATE-TEMACODE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Timeout.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Timeout.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.lib;
 
+import org.firstinspires.ftc.teamcode.lib.util.BooleanCommand;
+
+import static android.os.SystemClock.sleep;
+import static android.os.SystemClock.uptimeMillis;
+
 public class Timeout {
+    public static void waitUnlessInterrupt(long millis, BooleanCommand interrupt) {
+        long startingTime = uptimeMillis();
+        while (uptimeMillis() >= millis + startingTime || interrupt.execute()) {
+            sleep(1);
+        }
+    }
 }
