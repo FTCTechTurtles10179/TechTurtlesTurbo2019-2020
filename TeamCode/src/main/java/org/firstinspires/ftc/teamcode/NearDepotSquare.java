ff9049758db51c4d36e271f990eafe667d89b216
PurpleLaptop2 <techturtles10179@gmail.com>
2019-11-08 19:05:47 -0600

    renamed autos to match that there is only one per side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearDepotSquare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearDepotSquare.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.os.SystemClock;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.lib.AutoController;
 import org.firstinspires.ftc.teamcode.lib.Configurator;
 import org.firstinspires.ftc.teamcode.lib.WheelController;
 
-@Autonomous(name="blue1")
-public class blue1 extends LinearOpMode{
+import static java.lang.Thread.sleep;
+
+@Autonomous(name="NearDepotSquare")
+public class NearDepotSquare extends LinearOpMode {
     Configurator config;
     WheelController wheelController;
 
@@ -21,14 +26,21 @@ public class blue1 extends LinearOpMode{
         waitForStart();
         wheelController.runWithoutEncoder();
 
-        //moves sideways to accommodate for other bot
-        wheelController.moveXY(-0.5,0);
-        sleep(500);
+        //move to the foundation
+        wheelController.moveXY(0, -0.5);
+        sleep(800);
+
+        //stop at the foundation
         wheelController.stopWheels();
 
-        //moves forward under the skybridge
-        wheelController.moveXY(0,-0.5);
-        sleep(1250);
+        //deploy the foundation grabber
+        foundationGrabber.setPosition(1);
+
+        //move back to the loading zone
+        wheelController.moveXY(0, 0.5);
+        sleep(1100);
+
+        //park!
         wheelController.stopWheels();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearLoadingZoneTriangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearLoadingZoneTriangle.java
@@ -8,27 +8,23 @@ import org.firstinspires.ftc.teamcode.lib.AutoController;
 import org.firstinspires.ftc.teamcode.lib.Configurator;
 import org.firstinspires.ftc.teamcode.lib.WheelController;
 
-@Autonomous(name="blue2")
-public class blue2 extends LinearOpMode {
+@Autonomous(name="NearLoadingZoneTriangle")
+public class NearLoadingZoneTriangle extends LinearOpMode {
     Configurator config;
     WheelController wheelController;
 
-    public void runOpMode() {
+    public void runOpMode(){
         config = new Configurator(this);
         wheelController = new WheelController(config);
         Servo foundationGrabber;
         foundationGrabber = hardwareMap.servo.get("foundationGrabber");
         waitForStart();
-        wheelController.runWithoutEncoder();
 
-        //moves sideways to accommodate for other bot
-        wheelController.moveXY(0.5, 0);
-        sleep(500);
-        wheelController.stopWheels();
+        //moves to the skybridge
+        wheelController.moveXY(0,-0.5);
+        sleep(1200);
 
-        //moves forward under the skybridge
-        wheelController.moveXY(0, -0.5);
-        sleep(1250);
+        //park
         wheelController.stopWheels();
     }
 }
