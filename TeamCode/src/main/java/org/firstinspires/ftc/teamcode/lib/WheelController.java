447e4e84a5134789a42fc4f74b77ef096d90bbcc
PurpleLaptop2 <techturtles10179@gmail.com>
2019-11-08 18:13:13 -0600

    made wheelcontroller use movexyturn for other functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/WheelController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/WheelController.java
@@ -43,36 +43,11 @@ public class WheelController {
     }
 
     public void moveXY(double tx, double ty) {
-        double x = -Range.clip(tx, -1, 1);
-        double y = Range.clip(ty, -1, 1);
-
-
-        // The speed at which we will move the robot
-        double r = Math.hypot(-x, y);
-
-        // The angle at which the will move the robot
-        double robotAngle = Math.atan2(y, -x) - Math.PI / 4;
-
-        // Do the calculations for the wheel speeds
-        double v1 = r * Math.cos(robotAngle);
-        double v2 = r * Math.sin(robotAngle);
-        double v3 = r * Math.sin(robotAngle);
-        double v4 = r * Math.cos(robotAngle);
-
-        // Finally, take the math we did for the speed of the wheels and actually set the wheel's speed
-        frontLeft.setPower(-v1);
-        frontRight.setPower(-v2);
-        backLeft.setPower(-v3);
-        backRight.setPower(-v4);
+        moveXYTurn(tx, ty, 0);
     }
 
     public void moveTurn(double tspeed) {
-        double speed = Range.clip(tspeed, -1, 1);
-        // Just a simple tank turn, left goes forward and right goes backward
-        frontLeft.setPower(speed);
-        frontRight.setPower(-speed);
-        backLeft.setPower(speed);
-        backRight.setPower(-speed);
+        moveXYTurn(0,0,tspeed);
     }
 
     public void moveXYTurn(double tx, double ty, double tspeed) {
