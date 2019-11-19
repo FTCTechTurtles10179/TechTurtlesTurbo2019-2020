f0b0dc75e146b029c497511df36b27d1a82edc38
PurpleLaptop2 <techturtles10179@gmail.com>
2019-11-19 17:51:24 -0600

    made it so that configurator can handle errors better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Configurator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Configurator.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.lib;
 
+import com.qualcomm.hardware.HardwareFactory;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,43 +8,54 @@ import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Hardware;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Disabled
 public class Configurator {
-    Telemetry telemetry;
-    HardwareMap hardwareMap;
+    OpMode opMode;
 
     public Configurator(OpMode opMode) {
-        hardwareMap = opMode.hardwareMap;
-        telemetry = opMode.telemetry;
+        this.opMode = opMode;
     }
 
     public DcMotor getDcMotor(String name) {
         try {
-            return hardwareMap.dcMotor.get(name);
+            return opMode.hardwareMap.dcMotor.get(name);
         } catch (Exception e) {
-            telemetry.log().add("WARNING : Could not find motor " + name + ", please add to config.");
-            return hardwareMap.getAll(DcMotor.class).get(0);
+            opMode.telemetry.log().add("WARNING : Could not find motor " + name + ", please add to config.");
+            try {
+                return DcMotor.class.newInstance();
+            } catch (Exception ex) {
+                return null;
+            }
         }
     }
 
     public Servo getServo(String name) {
         try {
-            return hardwareMap.servo.get(name);
+            return opMode.hardwareMap.servo.get(name);
         } catch (Exception e) {
-            telemetry.log().add("WARNING : Could not find servo " + name + ", please add to config.");
-            return hardwareMap.getAll(Servo.class).get(0);
+            opMode.telemetry.log().add("WARNING : Could not find servo " + name + ", please add to config.");
+            try {
+                return Servo.class.newInstance();
+            } catch (Exception ex) {
+                return null;
+            }
         }
     }
 
     public TouchSensor getTouchSensor(String name) {
         try {
-            return hardwareMap.touchSensor.get(name);
+            return opMode.hardwareMap.touchSensor.get(name);
         } catch (Exception e) {
-            telemetry.log().add("WARNING : Could not find touchSensor " + name + ", please add to config.");
-            return hardwareMap.getAll(TouchSensor.class).get(0);
+            opMode.telemetry.log().add("WARNING : Could not find touchSensor " + name + ", please add to config.");
+            try {
+                return TouchSensor.class.newInstance();
+            } catch (Exception ex) {
+                return null;
+            }
         }
     }
 }
