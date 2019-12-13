b2c533ee3c954221ca88d373f7e4c5fc701ef147
PurpleLaptop2 <techturtles10179@gmail.com>
2019-12-13 17:47:16 -0600

    Turn adjusting for autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/AutonomousLibrary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/AutonomousLibrary.java
@@ -5,16 +5,21 @@ import org.firstinspires.ftc.teamcode.lib.util.State;
 public class AutonomousLibrary extends Configurator {
     double cmToClickForward = 16.5;
     double cmToClickRight = 24.3;
+    double turnDamping = 2;
+    double slowDown = 5;
+    double slowDist = 5;
+    double stopDist = 0.5;
 
     public void moveForwardCentimeters(double distance, double speed) {
         double startingEncoder = wheelController.avgEncoder();
         stateMachine.addState(new State(() -> {
-            if (Math.abs((wheelController.avgEncoder() - startingEncoder) - (distance * cmToClickForward)) >= 5 * cmToClickForward) {
-                wheelController.moveXY(0, speed);
+            double turnAdjust = (wheelController.leftEncoder() - wheelController.rightEncoder())/turnDamping;
+            if (Math.abs((wheelController.avgEncoder() - startingEncoder) - (distance * cmToClickForward)) >= slowDist * cmToClickForward) {
+                wheelController.moveXYTurn(0, speed, turnAdjust);
             } else {
-                wheelController.moveXY(0, speed/5);
+                wheelController.moveXYTurn(0, speed/slowDown, turnAdjust);
             }
-            boolean stop = (Math.abs((wheelController.avgEncoder() - startingEncoder) - (distance * cmToClickForward)) <= 0.5 * cmToClickForward);
+            boolean stop = (Math.abs((wheelController.avgEncoder() - startingEncoder) - (distance * cmToClickForward)) <= stopDist * cmToClickForward);
             if (debugMode) telemetry.addData("AutoLibStopWheels", stop);
             return stop;
         }, () -> {
@@ -25,12 +30,13 @@ public class AutonomousLibrary extends Configurator {
     public void moveRightCentimeters(double distance, double speed) {
         double startingEncoder = wheelController.rightEncoder();
         stateMachine.addState(new State(() -> {
-            if (Math.abs((wheelController.rightEncoder() - startingEncoder) - (distance * cmToClickRight)) >= 5 * cmToClickRight) {
-                wheelController.moveXY(speed, 0);
+            double turnAdjust = (wheelController.leftEncoder() - wheelController.rightEncoder())/turnDamping;
+            if (Math.abs((wheelController.rightEncoder() - startingEncoder) - (distance * cmToClickRight)) >= slowDist * cmToClickRight) {
+                wheelController.moveXYTurn(speed, 0, turnAdjust);
             } else {
-                wheelController.moveXY(speed/5, 0);
+                wheelController.moveXYTurn(speed/slowDown, 0, turnAdjust);
             }
-            boolean stop =(Math.abs((wheelController.rightEncoder() - startingEncoder) - (distance * cmToClickRight)) <= 0.5 * cmToClickRight);
+            boolean stop =(Math.abs((wheelController.rightEncoder() - startingEncoder) - (distance * cmToClickRight)) <= stopDist * cmToClickRight);
             if (debugMode) telemetry.addData("AutoLibStopWheels", stop);
             return stop;
         }, () -> {
