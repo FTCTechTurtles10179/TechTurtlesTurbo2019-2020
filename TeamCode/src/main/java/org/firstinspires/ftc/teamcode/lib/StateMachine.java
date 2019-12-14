813912216e616fd6078fcad6414ba5ed687dd49f
PurpleLaptop2 <techturtles10179@gmail.com>
2019-12-13 19:16:04 -0600

    StateMachine concurrentModificationException when adding states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/StateMachine.java
@@ -7,18 +7,25 @@ import java.util.List;
 
 public class StateMachine {
     List<State> states; //All of the current running states
+    List<State> statesToAdd; //States that need to be added to the list of running states
     Configurator config; //Configurator instance that this StateMachine is attached to
 
     StateMachine(Configurator config) {
         this.config = config;
+        statesToAdd = new ArrayList();
         states = new ArrayList();
     }
 
-    public void addState(State state) { //Add a state
-        states.add(state);
+    public void addState(State state) { //Add a state safely
+        statesToAdd.add(state);
     }
 
     public void runStates() {
+        for (State state: statesToAdd) {
+            states.add(state);
+        }
+        statesToAdd.clear();
+
         List<State> statesToRemove = new ArrayList<>();
         for (State state: states) {
             if (config.debugMode && state.getStateName() != "Hidden") config.telemetry.addLine("State: " + state.getStateName());
