a1eec64ddb7e8a78e80ce364e0d62b79aa9a85d3
PurpleLaptop2 <techturtles10179@gmail.com>
2019-12-23 13:24:09 -0600

    made and completed NearBlueSquare

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueSquareStackless.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.lib.AutonomousLibrary;
+import org.firstinspires.ftc.teamcode.lib.util.states.SingleState;
+import org.firstinspires.ftc.teamcode.lib.util.states.StartState;
+import org.firstinspires.ftc.teamcode.lib.util.states.State;
+
+@Autonomous(name="NearBlueSquareStackless")
+public class NearBlueSquareStackless extends AutonomousLibrary {
+    Servo foundationGrabber;
+
+    public void setupOpMode(){
+        foundationGrabber = getServo("foundationGrabber"); //Get the foundation grabber servo
+        foundationGrabber.setPosition(1);
+
+        State releaseStone = new SingleState(() -> {
+            foundationGrabber.setPosition(0);
+        }, "ReleaseStone");
+
+        State moveForwardToSkybridge = new SingleState(() -> {
+            moveForwardCentimeters(100, 0.75, releaseStone);
+        }, "MoveForwardToSkybridge");
+
+        State stopWheels = new State(() -> {
+            wheelController.stopWheels();
+            return false;
+        }, () -> {
+            stateMachine.addState(moveForwardToSkybridge);
+        }, "StopWheels");
+
+        State turnLeftTowardSkybridge = new State(() -> {
+            wheelController.moveTurn(1);
+            return false;
+        }, () -> {
+            stateMachine.addState(stopWheels);
+        }, 1000, "TurnLeftTowardSkybridge");
+
+        State grabStone = new State(() -> {
+            foundationGrabber.setPosition(1);
+            return false;
+        }, () -> {
+            stateMachine.addState(turnLeftTowardSkybridge);
+        }, 2000, "GrabStone");
+
+        State moveForwardToStone = new SingleState(() -> {
+            moveForwardCentimeters(80,0.5, grabStone);
+        }, "MoveForwardToStone");
+
+        stateMachine.addState(moveForwardToStone);
+    }
+}
