e74cac33bc6d0c50321242cd0b6111cd723ad4b3
PurpleLaptop2 <techturtles10179@gmail.com>
2020-01-09 16:43:15 -0600

    replaced turnLeftTowardSkybridge with moveBackFromStones and strafeRightToSkybridge and added a state to align with stones before moving to grab

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueSquareStackless.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueSquareStackless.java
@@ -16,39 +16,38 @@ public class NearBlueSquareStackless extends AutonomousLibrary {
         claw = getServo("claw"); //Get the foundation grabber servo
         claw.setPosition(1);
 
-        State releaseStone = new SingleState(() -> {
-            claw.setPosition(0);
-        }, "ReleaseStone");
+        State releaseStone = new SingleState(() ->
+            claw.setPosition(0)
+        , "ReleaseStone");
 
-        State moveForwardToSkybridge = new SingleState(() -> {
-            moveForwardCentimeters(100, 0.75, releaseStone);
-        }, "MoveForwardToSkybridge");
+        State strafeRightToSkybridge = new SingleState(() ->
+            moveRightCentimeters(100,0.75, releaseStone)
+        ,"StrafeRightToSkybridge");
 
-        State turnLeftTowardSkybridge = new State(() -> {
-            wheelController.moveTurn(1);
-            return false;
-        }, () -> {
-            wheelController.stopWheels();
-            stateMachine.addState(moveForwardToSkybridge);
-        }, 1000, "TurnLeftTowardSkybridge");
+        State moveBackFromStones = new SingleState(() ->
+            moveForwardCentimeters(20, -0.75, strafeRightToSkybridge)
+        , "MoveBackFromStones");
 
         State grabStone = new State(() -> {
             claw.setPosition(1);
             return false;
-        }, () -> {
-            stateMachine.addState(turnLeftTowardSkybridge);
-        }, 2000, "GrabStone");
+        }, () ->
+            stateMachine.addState(moveBackFromStones), 2000, "GrabStone");
 
-        State moveForwardToStone = new SingleState(() -> {
-            moveForwardCentimeters(80,0.5, grabStone);
-        }, "MoveForwardToStone");
+        State moveForwardToStone = new SingleState(() ->
+            moveForwardCentimeters(75,0.5, grabStone)
+        , "MoveForwardToStone");
+
+        State strafeRightToAlign = new SingleState(() ->
+            moveRightCentimeters(15,0.5,moveForwardToStone)
+        , "StrafeRightToAlign");
 
         State openClaw = new State(() -> {
             claw.setPosition(0);
             return false;
-        }, () -> {
-            stateMachine.addState(moveForwardToStone);
-        }, 2000, "OpenClaw");
+        }, () ->
+            stateMachine.addState(strafeRightToAlign)
+        , 2000, "OpenClaw");
 
         stateMachine.addState(openClaw);
     }
