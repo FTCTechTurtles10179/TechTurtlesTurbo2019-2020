1c2e2444d94dd8129b3f212d99beff3faa355ed0
PurpleLaptop2 <techturtles10179@gmail.com>
2020-01-11 15:03:34 -0600

    Overhauled near square autonomouses with odometry, ready for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueSquareStackless.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueSquareStackless.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.lib.AutonomousLibrary;
+import org.firstinspires.ftc.teamcode.lib.MecanumOdometer;
+import org.firstinspires.ftc.teamcode.lib.util.data.PVector;
 import org.firstinspires.ftc.teamcode.lib.util.states.SingleState;
 import org.firstinspires.ftc.teamcode.lib.util.states.State;
 
@@ -21,14 +23,27 @@ public class NearBlueSquareStackless extends AutonomousLibrary {
     public void setupOpMode(){
         claw = getServo("claw");//Get the claw servo
         armMotor = getDcMotor("armMotor");//Get the armMotor
-        claw.setPosition(0);//Open claw
+
+
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);//Set encoder value of armMotor to 0
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        claw.setPosition(0);//Open claw
         armMotor.setTargetPosition(armDownEncoder);//Lower arm
+
+        //Initialize starting position and rotation
+        PVector startingPos = new PVector(0,0);
+        initializeOdometry(startingPos,90);
+
+        //Set waypoints
+        PVector stone = new PVector(-75,7);
+        PVector backFromStones = new PVector(-55,7);
+        PVector triangleSide = new PVector(-55,142);
+        PVector underSkybridge = new PVector(-55, 122);
 
         State strafeLeftUnderSkybridge = new SingleState(() -> {//Creates a new SingleState, strafeLeftUnderSkybridge
             //Strafe left to Navigate
-            moveRightCentimeters(-20, -0.5);
+            setTargetXYRot(underSkybridge, 0);
         }, "StrafeLeftUnderSkybridge");//Name the state StrafeLeftUnderSkybridge
 
         State releaseStone = new State(() -> {//Creates a new state, releaseStone
@@ -39,36 +54,24 @@ public class NearBlueSquareStackless extends AutonomousLibrary {
             stateMachine.addState(strafeLeftUnderSkybridge);//Pass strafeLeftUnderSkybridge into the state machine
         },"ReleaseStone");//Name the state ReleaseStone
 
-        State strafeRightPastSkybridge = new SingleState(() -> {//Creates a new SingleState, strafeRightPastSkybridge
-            //Strafe right past the Skybridge and pass releaseStone into the state machine
-            moveRightCentimeters(135,0.5, releaseStone);
-        },"StrafeRightPastSkybridge");//Name the state StrafeRightPastSkybridge
-
-        State moveBackFromStones = new SingleState(() -> {//Creates a new SingleState, moveBackFromStones
-            //Move back from the stones and pass strafeRightPastSkybridge into the state machine
-            moveForwardCentimeters(-20, -0.5, strafeRightPastSkybridge);
-        }, "MoveBackFromStones");//Name the state MoveBackFromStones
+        State goToTriangleSide = new State(() -> {//Creates a new state, goToTriangleSide
+            setTargetXYRot(backFromStones, 0); //Move back from stone to avoid collision with skybridge
+            setTargetXYRot(triangleSide,0, releaseStone);//Move past the skybridge and pass releaseStone into the state machine
+            return false;
+        }, () -> {}, "GoToTriangleSide");//Name the state GoToTriangleSide
 
         State grabStone = new State(() -> {//Creates a new state, grabStone
             claw.setPosition(1);//Grab the stone
             armMotor.setTargetPosition(armUpEncoder);//Raise arm for easier transport
             return false;
         }, () -> {
-            stateMachine.addState(moveBackFromStones);//Pass moveBackFromStones into the state machine
+            stateMachine.addState(goToTriangleSide);//Pass goToTriangleSide into the state machine
         }, 2000, "GrabStone");//Name the state GrabStone and run for 2 seconds
 
+        State goToStone = new SingleState(() -> {//Creates a new SingleState, goToStone
+            setTargetXYRot(stone, 0, grabStone);//Line up with stone and pass grabStone into the state machine
+        }, "GoToStone");//Name the state GoToStone
 
-        State moveForwardToStone = new SingleState(() -> {//Creates a new SingleState, moveForwardToStone
-            //Move forward to the stone and pass grabStone into the state machine
-            moveForwardCentimeters(75, 0.5, grabStone);
-        }, "MoveForwardToStone");//Name the state MoveForwardToStone
-
-        State strafeRightToAlign = new SingleState(() -> {//Creates a new SingleState, strafeRightToAlign
-            //Strafe right slightly to align with the center stone in the right 3 stones
-            //Pass moveForwardToStone into the state machine
-            moveRightCentimeters(7, 0.5, moveForwardToStone);
-        }, "StrafeRightToAlign");//Name the state StrafeRightToAlign
-
-        stateMachine.addState(strafeRightToAlign);//Passes strafeRightToAlign into the state machine, calling it
+        stateMachine.addState(goToStone);//Passes goToStone into the state machine, calling it
     }
 }
