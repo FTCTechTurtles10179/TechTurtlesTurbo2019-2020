70440fef3be79b36045b7038481fba67e3dc4337
PurpleLaptop1 <techturtles10179@gmail.com>
2020-01-14 16:51:55 -0600

    near red triangle, IMU odometry, and updated the navigation of autolib

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/util/laptopspcific/RobotsPlease.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.lib.util.laptopspcific;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Robots, please!")
+public class RobotsPlease extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
