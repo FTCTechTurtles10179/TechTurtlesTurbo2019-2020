c289242b46670f739a9cfa3b29fa0f6fc060a371
Squirrles <techturtles10179@gmail.com>
2020-01-16 18:05:10 -0600

    created near blue triangle using pvector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueTriangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueTriangle.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.lib.AutonomousLibrary;
+import org.firstinspires.ftc.teamcode.lib.util.data.PVector;
 import org.firstinspires.ftc.teamcode.lib.util.states.StartState;
 import org.firstinspires.ftc.teamcode.lib.util.states.State;
 
@@ -14,43 +15,38 @@ public class NearBlueTriangle extends AutonomousLibrary {
     @Override
     public void setupOpMode() {
         foundationGrabber = getServo("foundationGrabber"); //Get the foundation grabber servo
-        foundationGrabber.setPosition(0.5);
+        foundationGrabber.setPosition(1);
 
-        State strafeRightToBridge = new StartState(() -> {
-            moveRightCentimeters(-120, -0.5);
-        }, () -> true, () -> {}, "StrafeRightToBridge");
+        initializeOdometry(new PVector(22.86, 265.76), -90);
+
+        State strafeToBridge = new StartState(() -> {
+            setTargetXYRot(new PVector(24.86, 230), -90);
+            setTargetXYRot(new PVector(32.86, 175.5), -90);
+        }, () -> true, () -> {}, "StrafeLeftToBridge");
 
         State releasePlatform = new State(() -> {
-            foundationGrabber.setPosition(1);
+            foundationGrabber.setPosition(1); //Pull up the foundation grabber servo
             return false;
-        }, () -> {
-            stateMachine.addState(strafeRightToBridge);
+        }, () -> { //When the state is done
+            stateMachine.addState(strafeToBridge); //Run this state
         }, 1000, "ReleasePlatform");
 
-        State moveBackwardFromPlatform = new StartState(() -> {
-            moveForwardCentimeters(110, 0.5, releasePlatform);
+        State pullBackFoundation = new StartState(() -> {
+            setTargetXYRot(new PVector(344.76, 265.76), -90, releasePlatform);
         }, () -> true, () -> {},"MoveBackwardFromPlatform");
 
         State grabPlatform = new State(() -> {
-            foundationGrabber.setPosition(0);
+            foundationGrabber.setPosition(0); //Push down the foundation grabber servo
             return false;
-        }, () -> {
-            stateMachine.addState(moveBackwardFromPlatform);
+        }, () -> { //When the state is done
+            stateMachine.addState(pullBackFoundation); //Run this state
         }, 1000, "GrabPlatform");
 
-        State moveForwardToPlatform = new StartState(() -> {
-            moveForwardCentimeters(-70, -0.5, grabPlatform);
+        State moveToPlatform = new StartState(() -> {
+            setTargetXYRot(new PVector(32.86, 265.76), -90);
+            setTargetXYRot(new PVector(152.86, 212.76), -90, grabPlatform);
         }, () -> true, () -> {}, "MoveForwardToPlatform");
 
-        State strafeAlign = new StartState(() -> {
-            wheelController.moveXY(0, 0.1);
-            moveRightCentimeters(40.5, 0.5, moveForwardToPlatform);
-        }, () -> true, () -> {}, "StrafeAlign");
-
-        State moveOffWall = new StartState(() -> {
-            moveForwardCentimeters(-8, -0.5, strafeAlign);
-        }, () -> true, () -> {}, "MoveForwardToPlatform");
-
-        stateMachine.addState(moveOffWall);
+        stateMachine.addState(moveToPlatform); //After we setup the states, add the first one to our stateMachine
     }
 }
