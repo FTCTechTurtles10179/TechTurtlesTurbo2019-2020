4716354e33e3c086dfa5d91a5b02b486a41a94b2
Ashley Trowell <ajtrowell@gmail.com>
2020-02-20 20:08:20 -0600

    Starting DeadWheelOdometry Tests
    Ashley was here.

---
+++ TeamCode/src/test/java/DeadWheelOdometerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeadWheelOdometerTest {
+
+    @Before
+    public void initialize() {
+
+
+    }
+
+    @Test
+    public void steppedTest() {
+
+    }
+}
